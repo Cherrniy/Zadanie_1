8b31c39cd1fc7809b04bb114fac5b863f7db5227
Cherrn <cherrniy.danil@gmail.com>
2020-06-10 14:33:51 +0300

    Добавьте файлы проекта.

---
+++ Задание №1 Курсовая ЯВУ/Zadanie_1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Задание__1_Курсовая_ЯВУ
+{
+    partial class Zadanie_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Zadanie_1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Launch = new System.Windows.Forms.Button();
+            this.R_value = new System.Windows.Forms.TextBox();
+            this.X_value = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Answer = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(252)))), ((int)(((byte)(251)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(320, 294);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.Answer);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(0, 247);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(316, 43);
+            this.panel2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Значение Y:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.Launch);
+            this.panel3.Controls.Add(this.R_value);
+            this.panel3.Controls.Add(this.X_value);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(0, 152);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(319, 89);
+            this.panel3.TabIndex = 1;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // Launch
+            // 
+            this.Launch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Launch.Location = new System.Drawing.Point(124, 63);
+            this.Launch.Name = "Launch";
+            this.Launch.Size = new System.Drawing.Size(75, 23);
+            this.Launch.TabIndex = 4;
+            this.Launch.Text = "Ok";
+            this.Launch.UseVisualStyleBackColor = true;
+            this.Launch.Click += new System.EventHandler(this.Launch_Click);
+            // 
+            // R_value
+            // 
+            this.R_value.Location = new System.Drawing.Point(124, 37);
+            this.R_value.Name = "R_value";
+            this.R_value.Size = new System.Drawing.Size(100, 20);
+            this.R_value.TabIndex = 3;
+            // 
+            // X_value
+            // 
+            this.X_value.Location = new System.Drawing.Point(124, 7);
+            this.X_value.Name = "X_value";
+            this.X_value.Size = new System.Drawing.Size(100, 20);
+            this.X_value.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Значение R:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Значение X:";
+            // 
+            // Answer
+            // 
+            this.Answer.AutoSize = true;
+            this.Answer.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Answer.Location = new System.Drawing.Point(120, 10);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(20, 22);
+            this.Answer.TabIndex = 5;
+            this.Answer.Text = "?";
+            // 
+            // Zadanie_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(320, 294);
+            this.Controls.Add(this.panel1);
+            this.Name = "Zadanie_1";
+            this.Text = "Zadanie_1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox R_value;
+        private System.Windows.Forms.TextBox X_value;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Launch;
+        private System.Windows.Forms.Label Answer;
+    }
+}
---
+++ Задание №1 Курсовая ЯВУ/Zadanie_1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание__1_Курсовая_ЯВУ
+{
+    public partial class Zadanie_1 : Form
+    {
+        public Zadanie_1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+//Нахатие на клавишу "Ok"
+        private void Launch_Click(object sender, EventArgs e)
+        {
+            String Y = "Ошибка";
+            double y = 0;
+            double x = double.Parse(X_value.Text);
+            double R = double.Parse(R_value.Text);
+            if (x <= -4)//Первый отрезок 
+            y = -3;
+              else
+              if (x > -4 && x <= -3)
+                 y = (2 * x) + 4;
+                 else
+                   if (x > -3 && x <= 3)
+                   y = Math.Sqrt(Math.Pow(R, 2) - Math.Pow(x, 2));
+                     else
+                       if (x > 3 && x <= 8)
+                       y = ((3 * x / 5) - 3);
+                          else
+                          if (x > 8)
+                            y = 3;
+            if (R != 3)
+                Answer.Text = Y.ToString();
+            else
+                Answer.Text = y.ToString();
+        }
+    }
+}
